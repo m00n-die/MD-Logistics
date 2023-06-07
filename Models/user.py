0ee7129ebb0e62e8e3335b0a17831c7dc594cb27
m00n-die <reginaldrock219>
2023-06-07 04:59:08 +0000

    more updates

---
+++ Models/user.py
@@ -0,0 +1,17 @@
+"""Lorem ipsum"""
+
+
+import uuid
+import sqlite3
+
+
+class User:
+    def __inti__(self, id, email, user_name, password, accountType):
+        self.id = uuid.uuid4()
+        self.email = email
+        self.user_name = user_name
+        self.password = password
+        self.accountType = accountType
+
+    def addUser(self, id, email, user_name, password, accountType):
+        if 
