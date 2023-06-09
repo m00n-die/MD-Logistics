8e2397a706ed813b616a8d3d3248e0f0dd92d03f
m00n-die <reginaldrock219>
2023-06-09 00:59:10 +0000

    added all user funcs

--- Models/user.py
+++ Models/user.py
@@ -3,16 +3,42 @@
 
 import uuid
 import sqlite3
+import re
 
+
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+def emailValidator(email):
+    """Function that checks and validates an email address
+    and stops execution of code if email is not valid"""
+    if not (re.fullmatch(regex, email)):
+        print("Invalid Email")
+        exit()
 
 class User:
-    def __inti__(self, id, email, user_name, password, accountType):
-        self.id = uuid.uuid4()
+    def __inti__(self, id=uuid.uuid4, userName="", email="", password="", accountType="user"):
+        self.id = id
+        self.userName = userName
         self.email = email
-        self.user_name = user_name
         self.password = password
         self.accountType = accountType
+        self.conn = sqlite3.connect("mdl.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS users (id TEXT, userName TEXT, email TEXT, password TEXT)")
+        self.conn.commit()
 
+    def addUser(self,id, userName, email, password):
+        self.cur.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (id, userName, email, password))
+        self.conn.commit()
+        print("success")
 
-    def addUser(self, id, email, user_name, password, accountType):
-        pass
+if __name__ == "__main__":
+    print("add user func, lets see")
+    print("1 add user")
+    choice = input("choice")
+    if choice == '1':
+        id = uuid.uuid4()
+        userName = input("user name")
+        email = input("email")
+        password = input("password")
+        User.addUser(id, userName, email, password)
--- main.py
+++ main.py
@@ -9,20 +9,38 @@ regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 conn = sqlite3.connect("mdl.db")
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS inventory (id INTEGER PRIMARY KEY, item_name TEXT, price REAL, item_qty INTEGER)")
-cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email TEXT, password TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, userName TEXT, email TEXT, password TEXT)")
 
 
-def email_check(email):
+def emailValidator(email):
     """Function that checks and validates an email address
     and stops execution of code if email is not valid"""
     if not (re.fullmatch(regex, email)):
         print("Invalid Email")
         exit()
 
-def add_user():
+def userNameValidator(userName):
+    userName = str(userName)
+    cur.execute("SELECT userName from users WHERE userName=?", (userName,))
+    exists = cur.fetchone()
+    if exists:
+        print("User Name already exists please try another one")
+        exit()
+
+def getResponse():
+    """Function that gets response from user and performs an operation
+    based on the response"""
+    response = input("Continue with delete operation?  Y/N: ")
+    if not response == "Y":
+        print("Operation cancelled")
+        exit()
+
+def addUser():
     """Function that adds a new user to the users table"""
+    userName = input("Please enter your User Name: ")
+    userNameValidator(userName)
     email = input("Enter your email: ")
-    email_check(email)
+    emailValidator(email)
     email_hash = hashlib.md5(email.encode())
     #print(email_hash.hexdigest())
     email_digest = email_hash.hexdigest()
@@ -34,7 +52,7 @@ def add_user():
     else:
         password = getpass.getpass(prompt="Enter a password: ")
         pass_hash = hashlib.md5(password.encode())
-        #print(pass_hash.hexdigest())
+        # print(pass_hash.hexdigest())
         confirm = getpass.getpass(prompt="Confirm your password: ")
         confirm_hash = hashlib.md5(confirm.encode())
         #print(confirm_hash.hexdigest())
@@ -45,34 +63,107 @@ def add_user():
             key_digest = key.hexdigest()
         else:
             print("Password mismatch")
-        cur.execute("INSERT INTO users VALUES (NULL, ?, ?)", (email_digest, key_digest))
+        cur.execute("INSERT INTO users VALUES (NULL, ?, ?, ?)", (userName, email_digest, key_digest))
         conn.commit()
 
-def login_user():
-    """Function that logs in an already existing user"""
-    email = input("Enter your email: ")
-    email_hash = hashlib.md5(email.encode())
-    email_digest = email_hash.hexdigest()
-    cur.execute("SELECT email FROM users WHERE email=?", (email_digest,))
+def loginUser():
+    """Logs in a user with their user name and password"""
+    userName = input("Enter your user name: ")
+    #email = input("Enter your email: ")
+    #email_hash = hashlib.md5(email.encode())
+    #email_digest = email_hash.hexdigest()
+    cur.execute("SELECT userName FROM users WHERE userName=?", (userName,))
     exists = cur.fetchone()
     if exists:
         password = getpass.getpass(prompt="Enter your password: ")
         password_hash = hashlib.md5(password.encode())
         #print(password_hash.hexdigest())
-        cur.execute("SELECT password FROM users WHERE email=?", (email_digest,))
-        valid_key = cur.fetchone()
+        cur.execute("SELECT password FROM users WHERE userName=?", (userName,))
+        validKey = cur.fetchone()
         #print(valid_key)
         key = ('{}'.format(password_hash.hexdigest()),)
         #print(key)
-        if key == valid_key:
+        if key == validKey:
             # fetchone() returns a tuple, so this is done to get the real value alone.
             print("Login Success")
+            loginUser.status = True
         else:
             print("Incorrect password. Try again.")
-            exit()
+            exit() 
     else:
-        print("Your email is not registered with an account. Please register")
+        print("No account is registered with user name. Please register")
         exit()
+    conn.commit()
+    
+def removeUser():
+    """Function that removes or deletes a user from the database"""
+    userName = input("Enter the user name of the account to be deleted: ")
+    cur.execute("SELECT userName FROM users WHERE userName=?", (userName,))
+    exists = cur.fetchone()
+    if exists:
+        print("Are you sure you want to delete user '{}'? This action cannot be undone!".format(userName))
+        getResponse()
+        cur.execute("DELETE FROM users WHERE userName=?", (userName,))
+        print("User: '{}' deleted".format(userName))
+    else:
+        print("There is no account registered with username: {}".format(userName))
+        exit()
+    conn.commit()
+
+def updateUserPassword():
+    """Function that updates the information or data on a user"""
+    print("Please login again with your old password.")
+    print("")
+    loginUser()
+    if loginUser.status == True:
+        email = input("Enter your account email: ")
+        emailValidator(email)
+        emailHash = hashlib.md5(email.encode())
+        emailDigest = emailHash.hexdigest()
+        cur.execute("SELECT email FROM users WHERE email=?", (emailDigest,))
+        exists = cur.fetchone()
+        if exists:
+            newPassword = getpass.getpass(prompt="Enter your new password: ")
+            newPasswordHash = hashlib.md5(newPassword.encode())
+            confirmPassword = getpass.getpass(prompt="Confirm password: ")
+            confirmPasswordHash = hashlib.md5(confirmPassword.encode())
+            if confirmPasswordHash.hexdigest() == newPasswordHash.hexdigest():
+                password = confirmPasswordHash.hexdigest()
+                cur.execute("UPDATE users SET password=? WHERE email=?", (password, emailDigest))
+                conn.commit()
+                print("Password successfully updated")
+            else:
+                print("Password mismatch")
+                    
+    else:
+        print("Login unsuccessful. Please try again")                
+
+def updateUserEmail():
+    """Function that updates the email of a user"""
+    print("Please login")
+    loginUser()  
+    if loginUser.status == True:
+        oldEmail = input("Enter your old email: ")
+        oldEmailHash = hashlib.md5(oldEmail.encode())
+        oldEmailDigest = oldEmailHash.hexdigest()
+        cur.execute("SELECT email FROM users WHERE email=?", (oldEmailDigest,))
+        exists = cur.fetchone()
+        if exists:
+            newEmail = input("Enter your new email address: ")
+            emailValidator(newEmail)
+            newEmailHash = hashlib.md5(newEmail.encode())
+            confirmEmail = input("Confirm new email: ")
+            emailValidator(confirmEmail)
+            confirmEmailHash = hashlib.md5(confirmEmail.encode())
+            if confirmEmailHash.hexdigest() == newEmailHash.hexdigest():
+                email = confirmEmailHash.hexdigest()
+                cur.execute("UPDATE users SET email=? WHERE email=?", (email, oldEmailDigest))
+                conn.commit()
+                print("Email successfully updated")
+            else:
+                print("Password mismatch")
+    else:
+        print("Login unsuccessful. Please try again")
 
 def add_item():
     """Function that adds an item to the inventory table"""
@@ -82,5 +173,8 @@ def add_item():
     cur.exectute("INSERT INTO inventory VALUES (NULL, ?, ?, ?)", (name, price, qty))
     
 
-#add_user()
-#login_user()
+# addUser()
+# loginUser()
+# removeUser()
+# updateUserPassword()
+# updateUserEmail()
